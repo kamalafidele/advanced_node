afddc4cfc34fd5f5a2d7cae8a68bc445d87e2a30
unknown <Kamalafizzet45@gmail.com>
2022-03-04 21:17:41 +0200

    added one more test

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -6,7 +6,7 @@ describe("calc", () =>{
     })
 });
 
-describe("get plans", () =>{
+describe("get todos", () =>{
     it("should return plans", (done) =>{
         request.get("https://jsonplaceholder.typicode.com/todos",(err,res) =>{
             expect(res.statusCode).toBe(200);
@@ -14,3 +14,9 @@ describe("get plans", () =>{
         })
     })
 });
+
+describe("check existence", () =>{
+    it("should check if the number exists in array ", () =>{
+        expect([4,12,56,90].includes(12)).toBeTrue();
+    })
+})
